3edc848c713977c3dac8fbc7d975f59dd0bb968c
MohdSajid12 <108975916+MohdSajid12@users.noreply.github.com>
2022-08-15 13:02:38 +0530

    Add files via upload

---
+++ Image-Carousel/script.js
@@ -0,0 +1,20 @@
+const imgs = document.getElementById('imgs')
+
+const img = document.querySelectorAll('#imgs imgs')
+
+let index = 0;
+
+
+function run(){
+
+    index++;
+
+    if(index > img.length -1){
+        index =0;
+    }
+ 
+    imgs.style.transform = `translateX(${-index*500}px)`;
+
+}
+
+setInterval(run , 2000)
